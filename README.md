7e8954003550041f1431b9cdf0e96a5014e8c193
“Jules <julessaysrawr@gmail.com>
2020-09-09 13:28:17 -0400

    feat: adding netlify-identity

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # netlify-identity
+
+[Tutorial](https://egghead.io/playlists/building-a-serverless-jamstack-todo-app-with-netlify-gatsby-graphql-and-faunadb-53bb)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Our Site</Heading>
-      <Button sx={{ marginTop: 2 }} onClick={() => {alert("clicked")}}>Log In</Button>
-    </Flex>
-  </Container>
-);
+export default (props) => {
+  useEffect(() => {
+    // using useEffect hook to get around document does not exist at build time in gatsby-ssr
+    // whenevr the component mounts, we'll run netlify identity once, and won't run it again
+    // useEffect won't run on the server, but will run in the browser
+
+    // have to deploy this before it works locally?
+    netlifyIdentity.init({});
+  });
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Our Site</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            console.log(netlifyIdentity.currentUser());
+          }}
+        >
+          Log User
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
