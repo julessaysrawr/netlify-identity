b852b35f76473c905ac930f84b9468195a11ebd3
“Jules <julessaysrawr@gmail.com>
2020-09-29 10:13:53 -0400

    feat: setting up for todos data

--- functions/graphql/index.js
+++ functions/graphql/index.js
@@ -3,16 +3,52 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 // Construct a schema, useing GraphQL schema language
 const typeDefs = gql`
 type Query {
-  hello: String
+  todos: [Todo]!
+}
+type Todo {
+  id: ID!
+  text: String!
+  done: Boolean!
+}
+type Mutation {
+  addTodo(text: String!): Todo
+  updateTodoDone(id: ID!): Todo
 }
 `;
+// ! means required
+
+const todos = {};
+let todoIndex = 0;
 
 // Provide resolver funtions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello World",
+    todos: () => {
+      return Object.values(todos);
+    }
   },
+  Mutation: {
+    addTodo: (_, {text}) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+      return todos[id];
+    },
+    updateTodoDone: (_, {id}) => {
+      todos[id].done = true;
+      return todos[id];
+    }
+
+  }
 };
+
+
+// Provide resolver funtions for your schema fields
+// const resolvers = {
+//   Query: {
+//     hello: () => "Hello World",
+//   },
+// };
 
 const server = new ApolloServer({
   typeDefs,
